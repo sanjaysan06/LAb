df13e25aa0663cfabd0b106a09d57a1ad720b30f
SANJAY <110025234+sanjaysan06@users.noreply.github.com>
2022-07-28 16:16:12 +0530

    Add files via upload

---
+++ Program/HelloWorld.java
@@ -0,0 +1,19 @@
+/*@Author: SANJAY S
+Date: 28/07/2022
+Title : HelloWorld
+Question : Executing Basic Java Program
+*/
+
+class HelloWorld
+{
+public static void main(String[]student)
+{
+System.out.println("Hello Dhanush");
+System.out.println(10+20);
+System.out.println("10"+"20");
+System.out.println(20*10);
+System.out.println(99-29);
+System.out.println("Wellcome to My Home");
+System.out.println("Back to Home");
+}
+}
