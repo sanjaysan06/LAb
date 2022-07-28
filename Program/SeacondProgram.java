dd9b23d843abc7f6cdada37afe1ae82f35505683
SANJAY <110025234+sanjaysan06@users.noreply.github.com>
2022-07-28 16:25:27 +0530

    Add files via upload

---
+++ Program/SeacondProgram.java
@@ -0,0 +1,36 @@
+/*@Author: SANJAY S
+Date: 28/07/2022
+Title : HelloWorld
+Question : Executing Basic Java Program
+*/
+
+class SeacondProgram
+{
+       public static void main(String[]Students)
+       { 
+
+            System.out.println("core java");
+
+            System.out.println("java 123");
+            
+			System.out.println(123456);
+
+            System.out.println("10 + 20");
+
+            System.out.println(1020142649);
+
+            System.out.println(true);//Boolean 
+			
+			System.out.println(false);//Boolean
+ 
+            System.out.println(10.20F);//floating
+
+            System.out.println(0b0011);//integer
+
+            System.out.println(0X01234abcd);//integer
+			
+		    System.out.println(0123456);//integer
+			
+		}	
+
+}		
